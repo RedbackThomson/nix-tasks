e710e86e8b92245605547149d79b50c24654e31b
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2026-02-05 14:38:05 -0800

    Remove flake-utils dependency

--- flake.nix
+++ flake.nix
@@ -3,51 +3,71 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      let
-        nix-tasks = pkgs.buildGoModule {
-          pname = "nix-tasks";
-          version = "0.1.0";
-          src = ./.;
-          vendorHash = "sha256-48Va8grh1BHGxZgwHKWvsB+HvBmmxD78cEDWl1Ysn4Y=";
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = fn: nixpkgs.lib.genAttrs systems (system: fn system);
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          nix-tasks = pkgs.buildGoModule {
+            pname = "nix-tasks";
+            version = "0.1.0";
+            src = ./.;
+            vendorHash = "sha256-48Va8grh1BHGxZgwHKWvsB+HvBmmxD78cEDWl1Ysn4Y=";
 
-          meta = with pkgs.lib; {
-            description = "Nix-based task runner and development environment manager";
-            homepage = "https://github.com/redbackthomson/nix-tasks";
-            license = licenses.mit;
-            mainProgram = "nix-tasks";
+            meta = with pkgs.lib; {
+              description = "Nix-based task runner and development environment manager";
+              homepage = "https://github.com/redbackthomson/nix-tasks";
+              license = licenses.mit;
+              mainProgram = "nix-tasks";
+            };
           };
-        };
-      in
-      {
-        packages = {
+        in
+        {
           default = nix-tasks;
           nix-tasks = nix-tasks;
-        };
+        }
+      );
 
-        apps.default = {
-          type = "app";
-          program = "${nix-tasks}/bin/nix-tasks";
-        };
+      apps = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          nix-tasks = self.packages.${system}.nix-tasks;
+        in
+        {
+          default = {
+            type = "app";
+            program = "${nix-tasks}/bin/nix-tasks";
+          };
+        }
+      );
 
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            go
-            gopls
-            golangci-lint
-            nix
-          ];
-        };
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              gopls
+              golangci-lint
+              nix
+            ];
+          };
+        }
+      );
 
-        # Export the library for consumers
-        lib = import ./lib { inherit pkgs; lib = pkgs.lib; };
-      }
-    );
+      lib = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        import ./lib { inherit pkgs; lib = pkgs.lib; }
+      );
+    };
 }
